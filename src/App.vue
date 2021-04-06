87ea969274b37f5636f3fe721f79f7fd4dee1b24
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-04-07 00:35:14 +0900

    JSX に変更

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
-
-<script lang="ts">
+<script lang="tsx">
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    return () => (
+      <>
+        <img alt="Vue logo" src="src/assets/logo.png" />
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </>
+    )
   }
 })
 </script>
